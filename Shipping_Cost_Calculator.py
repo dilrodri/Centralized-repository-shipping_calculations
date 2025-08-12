0277b12b419a21aa877fea5cda7cb81d8d4e88e5
dilrodri <dilrodri@gmail.com>
2025-08-12 18:20:33 -0400

    agregada línea adicional a Shipping_Cost_Calculator.py

--- Shipping_Cost_Calculator.py
+++ Shipping_Cost_Calculator.py
@@ -1,5 +1,6 @@
 # Shipping Cost Calculator
 # Here is a new update by dilrodri
+## Here is another update by dilrodri
 # This script calculates the total shipping cost based on weight and rate.
 
 # Prompt the user to enter the package weight (in kilograms)
